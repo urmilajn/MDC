a6d0745ccd37e8c0d483225718c3b09a2eeb6b99
Urmila JN <urmila.jnagesh@gmail.com>
2018-04-26 10:48:40 -0400

    admin > add user > working

--- routes/customer.js
+++ routes/customer.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Admin = require('../models/admin');
 const Customer = require('../models/customer');
+const User = require('../models/user');
 
 //View particular customer
 router.get('/', Admin.ensureAuthenticated, function(req, res){
@@ -29,9 +30,40 @@ router.get('/addUser', Admin.ensureAuthenticated, function(req, res){
 
 //Add User - Add new user for particular customer
 router.post('/addUser', Admin.ensureAuthenticated, function(req, res){
-	//---- Logic to add user to database
-	req.flash('success_msg', 'User added successfully');
-	res.redirect('/customer/manageUsers');
+	
+	var username = req.body.username;
+	var password = req.body.password;
+	var role = req.body.role;
+	var locations = req.body.locations;
+
+	console.log(role);
+
+	// Validation
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('role', 'Role is required').notEmpty();
+	req.checkBody('locations', 'At least one location is required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors)
+		res.render('addUser.handlebars', {errors: errors});
+	else {
+		var newUser = new User({
+			username: username,
+			password: password,
+			role: role,
+			locations: locations/*,
+			status: true*/
+		});
+
+		User.createUser(newUser, function(err,result){
+			if(err) throw err;
+		})
+
+		req.flash('success_msg', 'User added successfully');
+		res.redirect('/customer/manageUsers');
+	}	
 });
 
 //Cancel User - Add new user for particular customer
